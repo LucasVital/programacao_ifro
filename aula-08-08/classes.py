6125dfbc0806a7ca4479d6f53fc41aff43d85adb
Lucas Pedreira Vital <2021105100039@aluno.ifro.edu.br>
2022-08-15 20:41:13 -0400

    wip

--- aula-08-08/classes.py
+++ aula-08-08/classes.py
@@ -1,20 +1,55 @@
 class Conta:
+    __slots__ = [
+        '_numero',
+        '_titular',
+        '_saldo',
+        '_limite'
+    ]
 
-    def __init__(self, numero, titular, limite, saldo = 0):
+    def __init__(self, numero='', titular='', saldo=0.00, limite=0.00):
         self.numero = numero
         self.titular = titular
-        self.saldo = saldo
+        self._saldo = saldo
         self.limite = limite
 
+    @property
+    def numero(self):
+        return self._numero
+
+    @numero.setter
+    def numero(self, numero):
+        self._numero = ''
+        if not numero:
+            raise Exception("Informe o número")
+        self._numero = numero
+
+    @property
+    def titular(self):
+        return self._titular
+
+    @titular.setter
+    def titular(self, nome):
+        self._titular = ''
+        if len(nome) >= 5:
+            self._titular = nome
+
+    @property
+    def limite(self):
+        return self._limite
+
+    @limite.setter
+    def limite(self, limite):
+        self._limite = limite
+
     def deposita(self, valor):
-        self.saldo += valor
+        self._saldo += valor
 
     def saque(self, valor):
-        if valor <= self.saldo + self.limite:
-            self.saldo -= valor
-            if self.saldo < 0:
-                self.limite = self.limite + self.saldo
-                self.saldo = 0.00
+        if valor <= self._saldo + self._limite:
+            self._saldo -= valor
+            if self._saldo < 0:
+                self._limite = self._limite + self._saldo
+                self._saldo = 0.00
             return True
         return False
 
--- aula-08-08/testes.py
+++ aula-08-08/testes.py
@@ -1,6 +1,6 @@
 from classes import Conta
 
-conta1 = Conta(numero='4040-6', titular="Lucas", limite=10000.00, saldo=0.45)
+# conta1 = Conta(numero='4040-6', titular="Lucas", limite=10000.00, saldo=0.45)
 
 '''
 conta1.saldo = 10000.00
@@ -8,19 +8,27 @@ conta1.tipo = 'conta corrente'
 conta1.titular = 'Fulano de Tal'
 '''
 
-print(type(conta1))
-print(dir(conta1))
-print(conta1)
+# print(type(conta1))
+# print(dir(conta1))
+# print(conta1)
 
-conta3 = Conta(numero='78-6', limite=1000, titular='Sabrina')
-# conta3.limate = 3000
-print(conta3.titular)
+conta3 = Conta()
 
-conta3.deposita(30)
+conta3.limite = 6000
+conta3.numero = ''
 
-print(conta3.saldo)
-
-print(conta3.saque(4000))
-
-print(conta3.saldo)
 print(conta3.limite)
+print(conta3.numero)
+
+# print(conta3.titular)
+# conta3.limate = 3000
+# print(conta3.titular)
+# conta3.deposita(30)
+# print(conta3.saldo)
+# print(conta3.saque(4000))
+# print(conta3.saldo)
+# print(conta3.limite)
+# conta3._Conta__saldo = 5000
+# print(conta3._Conta__saldo)
+
+
